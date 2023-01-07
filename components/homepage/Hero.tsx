5927fcd5737fd74cdc88aa97b3c030c54e978ff3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-07 14:44:16 +0100

    creating a Hero component, adding animation

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -1,14 +1,15 @@
+import './Hero.scss';
+import LinesAnimation from '../animations/Lines';
+
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>School Locker</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto
-          dolore, impedit maiores illum amet repellendus ea consequatur vel
-          dolor eius, sapiente rem quod deleniti iste laudantium tenetur
-          quisquam. Dolorem, omnis.
-        </p>
+    <section className="hero-wrapper">
+      <div className="hero-title">
+        <h1>School</h1>
+        <div className="animation-wrapper">
+          <LinesAnimation />
+        </div>
+        <h1>Locker</h1>
       </div>
     </section>
   );
