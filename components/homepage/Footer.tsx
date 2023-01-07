7518c1f48134c72ffaae7481d9478f600a4b6cd2
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-07 11:09:07 +0100

    create footer

--- components/homepage/Footer.tsx
+++ components/homepage/Footer.tsx
@@ -1,17 +1,49 @@
 import './Footer.scss';
 import NewsmongerForm from '../newsmonger/NewsmongerForm';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <section className="footer-container">
       <NewsmongerForm />
-      <div>
-        <h4>new trends</h4>
-        <h4>school of rock</h4>
-        <h4>policy</h4>
-        <h4>earth</h4>
-      </div>
-    </footer>
+      <footer className="footer">
+        <div className="footer-links">
+          <div className="footer-list">
+            <h2>Company</h2>
+            <ul className="list-links">
+              <li>About Us</li>
+              <li>Pricing</li>
+              <li>Blog</li>
+              <li>Join Us</li>
+            </ul>
+          </div>
+          <div className="footer-list">
+            <h2>Legal</h2>
+            <ul className="list-links">
+              <li>Privacy Policy</li>
+              <li>Terms of use</li>
+              <li>Brand and Assets</li>
+              <li>Security and Privacy</li>
+            </ul>
+          </div>
+          <div className="footer-list">
+            <h2>Products</h2>
+            <ul className="list-links">
+              <li>
+                <Link href="/populate">Locker</Link>{' '}
+              </li>
+              <li>
+                <Link href="/wolfpad">Wolfpad</Link>{' '}
+              </li>
+              <li>
+                <Link href="/admin">Admin</Link>{' '}
+              </li>
+              <li>Editor</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </section>
   );
 };
 export default Footer;
