158dd13bffb073d8ba8f72c26476ed6547ddb937
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-08 14:54:33 +0100

    create TextEditor component with the plugins

---
+++ components/wolfpad/TextEditor/TextEditor.tsx
@@ -0,0 +1,99 @@
+import { useEffect } from 'react';
+import exampleTheme from './Themes';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
+import { ListItemNode, ListNode } from '@lexical/list';
+import { AutoLinkNode, LinkNode } from '@lexical/link';
+import { CodeHighlightNode, CodeNode } from '@lexical/code';
+import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
+import { ListPlugin } from '@lexical/react/LexicalListPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { TRANSFORMERS } from '@lexical/markdown';
+import prepopulatedText from './SampleText';
+import ToolbarPlugin from './Plugins/ToolbarPlugin';
+import CodeHighlightPlugin from './Plugins/CodeHighlightPlugin';
+import ActionsPlugin from './Plugins/ActionPlugin';
+// Lexical React plugins are React components, which makes them
+// highly composable. Furthermore, you can lazy load plugins if
+// desired, so you don't pay the cost for plugins until you
+// actually use them.
+import { testText } from './SampleText';
+
+const MyCustomAutoFocusPlugin = () => {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+};
+
+const Placeholder = () => {
+  return (
+    <div className="editor-placeholder">
+      Play around with the Markdown plugin...
+    </div>
+  );
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+const onError = (error: Error) => {
+  console.error(error.message);
+  throw error;
+};
+
+const TextEditor = () => {
+  const initialConfig = {
+    namespace: 'Wolfpad_Editor',
+    editorState: testText,
+    theme: exampleTheme,
+    onError,
+    // Any custom nodes go here
+    nodes: [
+      HeadingNode,
+      ListNode,
+      ListItemNode,
+      QuoteNode,
+      CodeNode,
+      CodeHighlightNode,
+      TableNode,
+      TableCellNode,
+      TableRowNode,
+      AutoLinkNode,
+      LinkNode,
+    ],
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-scroller">
+          <div className="editor-inner">
+            <RichTextPlugin
+              contentEditable={<ContentEditable className="editor-input" />}
+              placeholder={<Placeholder />}
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+            <MyCustomAutoFocusPlugin />
+            <ListPlugin />
+            <LinkPlugin />
+            <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+            <CodeHighlightPlugin />
+            <ActionsPlugin />
+          </div>
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+};
+export default TextEditor;
--- pages/wolfpad/index.tsx
+++ pages/wolfpad/index.tsx
@@ -1,6 +1,12 @@
 import CodeCell from '../../components/wolfpad/CodeCell/CodeCell';
+import TextEditor from '../../components/wolfpad/TextEditor/TextEditor';
 
 const PlaygroundPage = () => {
-  return <CodeCell />;
+  return (
+    <>
+      <TextEditor />
+      <CodeCell />
+    </>
+  );
 };
 export default PlaygroundPage;
