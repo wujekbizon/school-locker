d63143eed6cfe5ef8f598bc4d6d4bca010c66fa4
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-08 14:42:05 +0100

    adding lexical text editor framework and css styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/styles.css';
 import type { AppProps } from 'next/app';
 import type { Session } from 'next-auth';
 import Layout from '../components/layout/Layout';
