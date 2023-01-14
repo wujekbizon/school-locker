a384084cbccc683f095716bc0e8d4de00b0b4c6a
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-14 13:24:52 +0100

    add InputForm component, CSS styles and chatbot assets

---
+++ components/chat/InputForm.tsx
@@ -0,0 +1,30 @@
+import './InputForm.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type InputFormProps = {
+  onHandleSubmit: React.FormEventHandler<HTMLFormElement>;
+  formRef: React.MutableRefObject<HTMLFormElement | null>;
+};
+
+const InputForm = ({ onHandleSubmit, formRef }: InputFormProps) => {
+  return (
+    <form className="form" onSubmit={onHandleSubmit} ref={formRef}>
+      <button>
+        <Link href="/">
+          <Image src="/backarrow.svg" alt="back" width={25} height={25} />
+        </Link>
+      </button>
+      <textarea
+        name="prompt"
+        cols={1}
+        rows={1}
+        placeholder="Ask Jarvis..."
+      ></textarea>
+      <button className="send">
+        <Image src="/images/send.svg" alt="send" width={30} height={30} />
+      </button>
+    </form>
+  );
+};
+export default InputForm;
