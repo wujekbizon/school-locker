e8fc9484b10da89545d96f5ba4a280216f6e4524
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-23 09:00:08 +0100

    creating CustomFooter and MobileFooter

--- components/excalidraw/Excalidraw.tsx
+++ components/excalidraw/Excalidraw.tsx
@@ -1,4 +1,31 @@
 import './Excalidraw.scss';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import {
+  exportToCanvas,
+  exportToSvg,
+  exportToBlob,
+  exportToClipboard,
+  Excalidraw as Draw,
+  useHandleLibrary,
+  MIME_TYPES,
+  sceneCoordsToViewportCoords,
+  viewportCoordsToSceneCoords,
+  restoreElements,
+  LiveCollaborationTrigger,
+  MainMenu,
+  Footer,
+  Sidebar,
+} from '@excalidraw/excalidraw';
+import {
+  AppState,
+  BinaryFileData,
+  ExcalidrawImperativeAPI,
+  ExcalidrawInitialDataState,
+  Gesture,
+  LibraryItems,
+  PointerDownState as ExcalidrawPointerDownState,
+} from '@excalidraw/excalidraw/types/types';
+import { NonDeletedExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
 
 const Excalidraw = () => {
   return <div>Excalidraw</div>;
