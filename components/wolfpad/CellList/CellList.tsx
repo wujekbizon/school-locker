c8f02a7243d1f0eadeb5f2d571740ee3af503245
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-01-29 21:47:09 +0100

    adding CellList component

--- components/wolfpad/CellList/CellList.tsx
+++ components/wolfpad/CellList/CellList.tsx
@@ -1,4 +1,26 @@
+import styles from './CellList.module.css';
+import { useEffect, Fragment } from 'react';
+import { useTypedSelector } from '../../../hooks/useTypedSelector';
+import CellListItem from '../CellListItem/CellListItem';
+import AddCell from '../AddCell/AddCell';
+
 const CellList = () => {
-  return <div>CellList</div>;
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
+
+  return (
+    <div className={styles.cell_list}>
+      <AddCell prevCellId={null} forceVisible={cells.length === 0} />
+      {cells.map((cell) => {
+        return (
+          <Fragment key={cell.id}>
+            <CellListItem cell={cell} />
+            <AddCell prevCellId={cell.id} />
+          </Fragment>
+        );
+      })}
+    </div>
+  );
 };
 export default CellList;
